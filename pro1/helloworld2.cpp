88b9bf83ce1c33f1f2ff7d99e59111b68ba6484f
Rachit2000 <rachitchauhan2302@gmail.com>
2018-02-01 22:11:40 +0530

    second commit

--- pro1/helloworld2.cpp
+++ pro1/helloworld2.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main()
 {
 cout<<"helloworld"<<end;
+cout<<"welcome to my github profile"<<end;
+cout<<"i am a beginner"<<end;
+cout<<"creating origin and deleteing origin2"
+cout<<"thanks for watching my github account"<< end;
 return 0;
-}
-
 
