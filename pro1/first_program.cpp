8e0e68698461c30eb9e7f01eee58b3ccff93a79d
Rachit2000 <rachitchauhan2302@gmail.com>
2018-02-01 21:49:39 +0530

    first commit

---
+++ pro1/first_program.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int a = 5, b = 10, temp;
+cout<<"before swapping"<<end1;
+cout<<"a = "<< a <<", b = " << b << end1;
+temp = a;
+a =b;
+b = temp;
+cout << "after swapping"<< end1;
+cout <<"a = "<< a << ", b = " << b <<end1;
+return 0;
+}
+
+
---
+++ pro1/helloworld2.cpp
@@ -0,0 +1,9 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+cout<<"helloworld"<<end;
+return 0;
+}
+
+
